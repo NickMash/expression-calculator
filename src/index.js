4978871dd5b61296d9cbe6b459551ded8deb613d
Your Name <nicholasgrand@gmail.com>
2020-03-22 23:42:42 +0300

    added calculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let exprWithoutWhitespaces = expr.replace(/\s/g, "");
+    let arrOfSymbols = exprWithoutWhitespaces.split('');
+    let openBracketCount = 0;
+    let closeBracketCount = 0;
+
+    for (let symbol of arrOfSymbols){
+        if (symbol === "(") {
+            openBracketCount += 1;
+        }
+        else if (symbol === ')') {
+            closeBracketCount += 1;
+        }
+    }
+
+    if (openBracketCount !== closeBracketCount) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+
+    for (let i = 0 ; i < arrOfSymbols.length ; i ++){
+        if (arrOfSymbols[i] === "/" &&
+            arrOfSymbols[i+1] === "0") {
+            throw new Error('TypeError: Division by zero.');
+        }
+    }
+    let result = new Function("return " + exprWithoutWhitespaces);
+    return(result());
 }
 
 module.exports = {
